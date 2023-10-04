709819a59e388510fd9867df7b53413e91650f77
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-10-04 17:10:12 -0400

    swipe left/right animation; add 2 buttons function

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
-import RestaurantCard from "./RestaurantCard";
-import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+// import RestaurantCard from "./RestaurantCard";
+import { useEffect, useState, useRef } from "react";
 import Swiper from "react-native-deck-swiper";
 
 import ENV from "./env";
@@ -11,6 +11,8 @@ export default function App() {
   const [businesses, setBusinesses] = useState([]);
   //CurrentIndex is the index of businesses, it will change when card go previous/Next.
   const [currentIndex, setCurrentIndex] = useState(0);
+  const swiperRef = useRef();
+
   const loadData = async () => {
     const apiKey = ENV.YELP_API_KEY;
     const options = {
@@ -38,23 +40,59 @@ export default function App() {
   useEffect(() => {
     loadData();
   }, []);
-  console.log(businesses[0]);
+  // console.log(businesses[0]);
+  const swipeLeft = () => {
+    if (currentIndex > 0) {
+      swiperRef.current.swipeLeft();
+    }
+  };
+
+  const swipeRight = () => {
+    if (currentIndex < businesses.length - 1) {
+      swiperRef.current.swipeRight();
+    } else {
+      // Refill cards with new data from the API
+      loadData(); // Call the function to fetch data again
+    }
+  };
   return (
     <View>
       <Swiper
+        ref={swiperRef}
         cards={businesses.map((business) => business.name)}
         renderCard={(card) => {
-          const restaurant = businesses[0];
+          const restaurant = businesses[currentIndex];
           //prevent rendering restaurant.name, url before restaurant is logged.
           if (!restaurant) {
             console.log("nothing is populated yet");
             return null; // Return null if restaurant is not defined yet
           }
+          console.log(currentIndex);
           return (
-            <RestaurantCard style={styles.container} restaurant={restaurant} />
+            <View style={styles.card}>
+              <Image
+                style={styles.cardImage}
+                source={{ uri: restaurant.image_url }}
+              />
+              <Text style={styles.cardText}>{restaurant.name}</Text>
+              <Text style={styles.cardRating}>Rating: {restaurant.rating}</Text>
+            </View>
           );
         }}
+        onSwipedLeft={() => setCurrentIndex((prevIndex) => prevIndex - 1)}
+        onSwipedRight={() => setCurrentIndex((prevIndex) => prevIndex + 1)}
+        cardIndex={currentIndex}
+        backgroundColor="white"
+        stackSize={1}
       />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={swipeLeft}>
+          <Text style={styles.buttonText}>Previous</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={swipeRight}>
+          <Text style={styles.buttonText}>Next</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -62,8 +100,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+  },
+  card: {
+    width: 300,
+    height: 300,
+    borderRadius: 10,
+    backgroundColor: "lightblue",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cardImage: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  cardText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  cardRating: {
+    fontSize: 18,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+  },
+  button: {
+    backgroundColor: "skyblue",
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
--- RestaurantCard.js
+++ RestaurantCard.js
@@ -1,18 +1,55 @@
 import * as React from "react";
 import { Button, Card, Text } from "react-native-paper";
-
+import { View, Image, StyleSheet } from "react-native";
 const RestaurantCard = ({ restaurant }) => (
-  <Card>
-    <Card.Content>
-      <Text variant="titleLarge">Restaurant Name: {restaurant.name}</Text>
-      <Text variant="bodyMedium">Rating: {restaurant.rating}</Text>
-    </Card.Content>
-    <Card.Cover source={{ uri: restaurant.image_url }} />
-    <Card.Actions>
-      <Button>Previous</Button>
-      <Button>Next</Button>
-    </Card.Actions>
-  </Card>
+  <View style={styles.card}>
+    <Image style={styles.cardImage} source={{ uri: restaurant.image_url }} />
+    <Text style={styles.cardText}>{restaurant.name}</Text>
+    <Text style={styles.cardRating}>Rating: {restaurant.rating}</Text>
+  </View>
 );
 
 export default RestaurantCard;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  card: {
+    width: 300,
+    height: 300,
+    borderRadius: 10,
+    backgroundColor: "lightblue",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cardImage: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  cardText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  cardRating: {
+    fontSize: 18,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+  },
+  button: {
+    backgroundColor: "skyblue",
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+});
