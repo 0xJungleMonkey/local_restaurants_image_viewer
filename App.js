246d58c9f3caf8de178cb499f9691c8849c70d13
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-10-04 15:30:42 -0400

    React hook to fetch data from api, test in console

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import RestaurantCard from "./RestaurantCard";
+import { useEffect, useState } from "react";
 export default function App() {
+  const [businesses, setBusinesses] = useState([]);
+  const loadData = async () => {
+    const apiKey = ENV.YELP_API_KEY;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: "Bearer " + apiKey,
+      },
+    };
+    try {
+      let response = await fetch(
+        `https://api.yelp.com/v3/businesses/search?location=New%20York%20City&sort_by=best_match&limit=20`,
+        options
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not OK");
+      }
+      const data = await response.json();
+      setBusinesses(data.businesses);
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error);
+    }
+  };
+  useEffect(() => {
+    loadData();
+  }, []);
   return (
     <View>
-      <RestaurantCard />
+      <RestaurantCard style={styles.container} />
     </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- RestaurantCard.js
+++ RestaurantCard.js
@@ -4,13 +4,13 @@ import { Button, Card, Text } from "react-native-paper";
 const RestaurantCard = () => (
   <Card>
     <Card.Content>
-      <Text variant="titleLarge">Card title</Text>
-      <Text variant="bodyMedium">Card content</Text>
+      <Text variant="titleLarge">Restaurant Name: </Text>
+      <Text variant="bodyMedium">Rating: </Text>
     </Card.Content>
     <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
     <Card.Actions>
-      <Button>Cancel</Button>
-      <Button>Ok</Button>
+      <Button>Previous</Button>
+      <Button>Next</Button>
     </Card.Actions>
   </Card>
 );
